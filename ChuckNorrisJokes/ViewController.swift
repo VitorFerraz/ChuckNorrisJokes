7549a907c1eef1d6857edbf28db7876081977448
Vitor Ferraz Varela <vitorfvarela@gmail.com>
2020-06-26 17:17:42 -0300

    Initial Commit

---
+++ ChuckNorrisJokes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChuckNorrisJokes
+//
+//  Created by Vitor Ferraz Varela on 26/06/20.
+//  Copyright © 2020 Vitor Ferraz Varela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
